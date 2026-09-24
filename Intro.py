3d89e031341517ab1074a33c28058400d8f5a5f9
Jjlopezp2 <juanjoseabcd.jj@gmail.com>
2026-09-23 19:55:41 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -20,31 +20,36 @@ with col1:
  
  st.subheader("Conversión de texto a voz")
  image = Image.open('txt_to_audio2.jpg')
- st.write("En la siguiente enlace usaremos una de las aplicaciones de Inteligencia Artificial") 
+ st.image(image, width=192)
+ st.write("En el siguiente enlace, se convertirá un texto a audio.") 
  url = "https://e487tvzsuvluweowgtrjlp.streamlit.app/"
  st.write(f"Texto a voz: [Enlace]({url})")
 
  st.subheader("Conversión de Imagen a Texto")
  image = Image.open('unnamed1.png')
- st.write("En la siguiente enlace veremos como se detectan objetos en Imágenes.") 
+ st.image(image, width=192)
+ st.write("En el siguiente enlace, se podrá convertir imágenes a texto.") 
  url = "https://ocr-audio-jmv5lbb3xvkt3kh3cmygxx.streamlit.app/"
- st.write(f"YOLO: [Enlace]({url})")
+ st.write(f"Imagen a Texto: [Enlace]({url})")
 
  st.subheader("Traductor de Audio")
  image = Image.open('unnamed.png')
- st.write("En la siguiente enlace veremos como puedes usar tu modelo entrenado.") 
+ st.image(image, width=192)
+ st.write("En el siguiente enlace, se podrá traducir un audio a otro idioma.") 
  url = "https://shdksjdks-4gcjnnv6zdf5urwsw5nsje.streamlit.app/"
  st.write(f"Traductor de Audio: [Enlace]({url})")
 
 with col2: 
  st.subheader("Reconocimiento de Imágenes")
  image = Image.open('image-3-2.png')
- st.write("En la siguiente veremos una aplicación que usa la conversión de voz a texto.") 
+ st.image(image, width=200)
+ st.write("En el siguiente enlace, se podrán reconocer objetos de un ambiente.") 
  url = "https://kkt9hzt2g6ridht6gxne5q.streamlit.app/"
  st.write(f"Reconocimiento de Imágenes: [Enlace]({url})")
 
  st.subheader("Giving up")
  image = Image.open('d086f56a4da1d8aedad7840d84273007.jpg')
+ st.image(image, width=200)
  st.write("En el siguiete enlace vamos a ver una app acerca de rendirse completamente.") 
  url = "https://jjlopezp2-whatev-whatev-xq31nq.streamlit.app/"
  st.write(f"Give Up: [Enlace]({url})")
